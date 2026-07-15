8529bb4b4f4cefe085dbf66c44300b8a39ecf75e
MicheleTodeschini <micheletodeschini01@gmail.com>
2026-07-15 15:55:50 +0200

    format secifiers

---
+++ HelloWorld/11. format specifiers/format_specifiers.py
@@ -0,0 +1,9 @@
+
+
+price1 = 3.14159
+price2 = -987.65
+price3 = 12000.34
+
+print(f'Price1 is {price1:.2f}')
+print(f'Price2 is {price2:10}') # ha 10 spazi per far visualizzare il numero
+print(f'Price3 is {price3:+,.2f}') # mette il + davanti se il numerò è positivo, la , per le migliaia e poi solamente 2 cifre decimali
