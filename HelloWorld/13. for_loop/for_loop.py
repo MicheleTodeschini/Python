f415cb6a1d65040b37bf598713643fb233b1c348
MicheleTodeschini <micheletodeschini01@gmail.com>
2026-07-15 16:33:22 +0200

    for loop

---
+++ HelloWorld/13. for_loop/for_loop.py
@@ -0,0 +1,19 @@
+
+
+
+""" for i in range(1, 11):
+    print(i) """
+
+""" for i in reversed(range(1, 11)):
+    print(i)
+
+print('Happy new year') """
+
+""" for i in range(1, 11, 2):  # A numeri alterni, ogni 2 numeri
+    print(i) """
+
+for i in range(1, 21):
+    if i == 13:
+        break
+    else:
+        print(i)
