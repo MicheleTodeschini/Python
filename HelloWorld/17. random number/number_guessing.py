892d8e8e63b6a20fbd6731e8e2f853e0da4df7cb
MicheleTodeschini <micheletodeschini01@gmail.com>
2026-07-16 15:24:49 +0200

    number guessing game

---
+++ HelloWorld/17. random number/number_guessing.py
@@ -0,0 +1,31 @@
+import random
+
+lowest_num = 1
+highest_num = 100
+answer = random.randint(lowest_num, highest_num)
+guesses = 0
+is_running = True
+
+print('Python number guessing game')
+print(f'Select a number between {lowest_num} and {highest_num}: ')
+
+while is_running:
+    guess = input('Enter your guess: ')
+
+    if guess.isdigit():
+        guess = int(guess)
+        guesses += 1
+
+        if guess < lowest_num or guess > highest_num:
+            print('that number is out of range')
+            print(f'Please select a number between {lowest_num} and {highest_num}: ')
+        elif guess < answer:
+            print('Too low, try again')
+        elif guess > answer:
+            print('Too high, try again')
+        else:
+            print(f'Correct! the anser was {answer}')
+            print(f'Answer of guesses is: {guesses}')
+    else:
+        print('Invalid guess')
+        print(f'Please select a number between {lowest_num} and {highest_num}: ')
---
+++ HelloWorld/17. random number/random_number.py
@@ -0,0 +1,9 @@
+import random
+
+""" number = random.randint(1, 6) """
+
+""" options = ('rock', 'paper', 'scissors')
+
+option = random.choice(options)
+
+print(option) """
