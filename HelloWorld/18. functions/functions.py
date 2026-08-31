16031b292c9eac2d4aeb8354270db76ccb18d9ba
MicheleTodeschini <micheletodeschini01@gmail.com>
2026-08-31 21:15:48 +0200

    f

---
+++ HelloWorld/18. functions/functions.py
@@ -0,0 +1,9 @@
+def happy_birthday(name):
+    print(f'Happy birthday to {name}!')
+    print('You are old!')
+    print('Happy birthday to you!')
+    print()
+
+happy_birthday('Michele')
+happy_birthday('Pierino')
+ fdfd
