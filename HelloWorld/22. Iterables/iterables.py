40f4e7c3345e129fe553679ff954e1e32dcaffbc
MicheleTodeschini <micheletodeschini01@gmail.com>
2026-09-01 22:20:13 +0200

    iterables

---
+++ HelloWorld/22. Iterables/iterables.py
@@ -0,0 +1,18 @@
+# Iterables = An objext/collection that can return its elements one at a time
+
+""" numbers = [1, 2, 3, 4, 5]
+
+for number in reversed(numbers):
+    print(number, end="-" ) """
+
+
+""" fruits = {"apple", "banana", "orange"}
+
+for fruit in fruits:
+    print(fruit)  """
+
+my_dictionary = {"A": 1, "B": 2, "C": 3}
+
+
+for key, value in my_dictionary.items():
+    print(key, value)
